16d38b7f914c994f11c6cb8bd8e0a33a460fd135
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-06-19 16:36:19 -0300

    Media Query practice - CSS Layout

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// copyright year
+document.querySelector("#year").textContent = new Date().getFullYear();
+
+// Get the last time modified of the document
+document.querySelector(
+  "#lastModified"
+).innerHTML = `Last Modification: ${document.lastModified}`;
